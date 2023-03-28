0e7eb3290d01275ca456ebb8472b25376bc56275
FAUCHER Thomas <o22101900@info24-12.iut45.univ-orleans.fr>
2023-03-28 14:10:44 +0200

    Creation de la base et des insertions

--- insertion.sql
+++ insertion.sql
@@ -1,14 +1,31 @@
-INSERT INTO Compagnie (nom_compagnie)
-VALUES ('Air France'), ('Lufthansa'), ('British Airways');
+-- insertion des données dans la table Compagnie
+INSERT INTO Compagnie (nom_compagnie) VALUES ('Air France');
+INSERT INTO Compagnie (nom_compagnie) VALUES ('Lufthansa');
+INSERT INTO Compagnie (nom_compagnie) VALUES ('British Airways');
 
-INSERT INTO Aeroport (nom_aeroport, ville, pays)
-VALUES ('Charles de Gaulle', 'Paris', 'FR'),
-       ('Antonio Carlos Jobim', 'Rio de Janeiro', 'BR'),
-       ('Heathrow', 'London', 'UK'),
-       ('Frankfurt Airport', 'Frankfurt', 'DE');
+-- insertion des données dans la table Aeroport
+INSERT INTO Aeroport (nom_aeroport, ville, pays) VALUES ('Charles de Gaulle', 'Paris', 'FR');
+INSERT INTO Aeroport (nom_aeroport, ville, pays) VALUES ('Antonio Carlos Jobim', 'Rio de Janeiro', 'BR');
+INSERT INTO Aeroport (nom_aeroport, ville, pays) VALUES ('Heathrow', 'London', 'UK');
+INSERT INTO Aeroport (nom_aeroport, ville, pays) VALUES ('Frankfurt Airport', 'Frankfurt', 'DE');
+
+-- insertion des données dans la table Vol
+INSERT INTO Vol (numero_vol, ID_compagnie, date_heure_depart, ID_aeroport_depart, terminal_depart, ID_aeroport_arrivee, terminal_arrivee, date_heure_arrivee)
+SELECT 0442, c.ID_compagnie, TO_TIMESTAMP('2023-07-10 23:30:00', 'YYYY-MM-DD HH24:MI:SS'), a.ID_aeroport, '2E', 2, '1', TO_TIMESTAMP('2023-07-11 05:30:00', 'YYYY-MM-DD HH24:MI:SS')
+FROM Compagnie c, Aeroport a
+WHERE c.nom_compagnie = 'Air France' AND a.ville = 'Paris';
 
 INSERT INTO Vol (numero_vol, ID_compagnie, date_heure_depart, ID_aeroport_depart, terminal_depart, ID_aeroport_arrivee, terminal_arrivee, date_heure_arrivee)
-VALUES (0442, 1, '2023-07-10 23:30:00', 1, '2E', 2, '1', '2023-07-11 05:30:00'),
-       (0356, 1, '2023-07-10 20:00:00', 1, '2F', 3, '5', '2023-07-10 21:30:00'),
-       (1215, 2, '2023-07-10 18:00:00', 1, '1A', 4, '3', '2023-07-10 19:45:00'),
-       (7812, 3, '2023-07-10 07:45:00', 3, '4', 4, '2', '2023-07-10 10:30:00');
+SELECT 0356, c.ID_compagnie, TO_TIMESTAMP('2023-07-10 20:00:00', 'YYYY-MM-DD HH24:MI:SS'), a.ID_aeroport, '2F', 3, '5', TO_TIMESTAMP('2023-07-10 21:30:00', 'YYYY-MM-DD HH24:MI:SS')
+FROM Compagnie c, Aeroport a
+WHERE c.nom_compagnie = 'Air France' AND a.nom_aeroport = 'Heathrow';
+
+INSERT INTO Vol (numero_vol, ID_compagnie, date_heure_depart, ID_aeroport_depart, terminal_depart, ID_aeroport_arrivee, terminal_arrivee, date_heure_arrivee)
+SELECT 1215, c.ID_compagnie, TO_TIMESTAMP('2023-07-10 18:00:00', 'YYYY-MM-DD HH24:MI:SS'), a.ID_aeroport, '1A', 4, '3', TO_TIMESTAMP('2023-07-10 19:45:00', 'YYYY-MM-DD HH24:MI:SS')
+FROM Compagnie c, Aeroport a
+WHERE c.nom_compagnie = 'Lufthansa' AND a.ville = 'Paris';
+
+INSERT INTO Vol (numero_vol, ID_compagnie, date_heure_depart, ID_aeroport_depart, terminal_depart, ID_aeroport_arrivee, terminal_arrivee, date_heure_arrivee)
+SELECT 7812, c.ID_compagnie, TO_TIMESTAMP('2023-07-10 07:45:00', 'YYYY-MM-DD HH24:MI:SS'), a.ID_aeroport, '4', 4, '2', TO_TIMESTAMP('2023-07-10 10:30:00', 'YYYY-MM-DD HH24:MI:SS')
+FROM Compagnie c, Aeroport a
+WHERE c.nom_compagnie = 'British Airways' AND a.pays = 'DE';
--- script.sql
+++ script.sql
@@ -1,38 +1,58 @@
-DROP DATABASE IF EXISTS COMPAGNIE_AERIENNE;
-CREATE DATABASE COMPAGNIE_AERIENNE DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE COMPAGNIE_AERIENNE;
+-- Supprimer les tables si elles existent déjà
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE Vol';
+   EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+/
 
-SET foreign_key_checks = 0;
-drop table if exists CIA, VOL, AEROPORT, AVION, SIEGE, CLASSE, CLIENT, PRIX_VOL, RES_CLIENT;
-SET foreign_key_checks = 1;
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE Aeroport';
+   EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+/
 
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE Compagnie';
+   EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+/
+
+-- Créer les tables
 CREATE TABLE Compagnie (
-    ID_compagnie INT AUTO_INCREMENT PRIMARY KEY,
-    nom_compagnie VARCHAR(255) NOT NULL
+    ID_compagnie NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nom_compagnie VARCHAR2(255) NOT NULL
 );
 
 CREATE TABLE Aeroport (
-    ID_aeroport INT AUTO_INCREMENT PRIMARY KEY,
-    nom_aeroport VARCHAR(255) NOT NULL UNIQUE,
-    ville VARCHAR(255) NOT NULL,
-    pays VARCHAR(255) NOT NULL
+    ID_aeroport NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nom_aeroport VARCHAR2(255) NOT NULL UNIQUE,
+    ville VARCHAR2(255) NOT NULL,
+    pays VARCHAR2(255) NOT NULL
 );
 
 CREATE TABLE Vol (
-    ID_vol INT AUTO_INCREMENT PRIMARY KEY,
-    numero_vol INT NOT NULL,
-    ID_compagnie INT,
-    date_heure_depart DATETIME NOT NULL,
-    ID_aeroport_depart INT,
-    terminal_depart VARCHAR(255) NOT NULL,
-    ID_aeroport_arrivee INT,
-    terminal_arrivee VARCHAR(255) NOT NULL,
-    date_heure_arrivee DATETIME NOT NULL,
+    ID_vol NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    numero_vol NUMBER NOT NULL,
+    ID_compagnie NUMBER,
+    date_heure_depart DATE NOT NULL,
+    ID_aeroport_depart NUMBER,
+    terminal_depart VARCHAR2(255) NOT NULL,
+    ID_aeroport_arrivee NUMBER,
+    terminal_arrivee VARCHAR2(255) NOT NULL,
+    date_heure_arrivee DATE NOT NULL,
     FOREIGN KEY (ID_compagnie) REFERENCES Compagnie (ID_compagnie),
     FOREIGN KEY (ID_aeroport_depart) REFERENCES Aeroport (ID_aeroport),
     FOREIGN KEY (ID_aeroport_arrivee) REFERENCES Aeroport (ID_aeroport)
 );
-    
-
-
-
